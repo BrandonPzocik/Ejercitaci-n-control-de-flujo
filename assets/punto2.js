99865ccaee23f042c042865c6e3b3f772160610b
Brandon <pzocikbrandon@gmail.com>
2024-05-13 11:32:52 -0300

    Se termino punto 2

---
+++ assets/punto2.js
@@ -0,0 +1,17 @@
+/*Triangulo de #. Para este ejercicio, construir un programa que permita armar un trianguloformado con #, de la siguiente manera:
+#
+##
+###
+####
+#####*/
+const altura = 5
+let triangulo = []
+let i = 0 
+
+ for (i;i <= altura; i++) {
+    let linea = Array(i).fill('#').join('');
+    triangulo.push(linea);
+ }
+
+triangulo.forEach(linea => console.log(linea));
+
